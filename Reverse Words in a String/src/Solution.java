9b499b2f087881c632911e34ab8340c6e47a2b5f
Kai Chen <allench86@gmail.com>
2014-08-18 23:38:27 -0700

    Reverse Words in a String
    Approach 2:
    1. reverse the whole string
    2. reverse every word in the reversed string

--- Reverse Words in a String/src/Solution.java
+++ Reverse Words in a String/src/Solution.java
@@ -1,40 +1,41 @@
 public class Solution {
 	public String reverseWords(String s) {
+		String result = reverseString(s, 0, s.length() - 1);
+		int startIndex = 0;
+		int endIndex = result.indexOf(' ', startIndex);
+		while (endIndex != -1) {
+			result = reverseString(result, startIndex, endIndex - 1);
+			startIndex = endIndex + 1;
+			endIndex = result.indexOf(' ', startIndex);
+		}
+		result = reverseString(result, startIndex, result.length() - 1);
+		return result;
+	}
+
+	public String reverseString(String s, int startIndex, int endIndex) {
 		if (s == null || s.isEmpty()) {
 			return "";
 		}
-		StringBuilder sb = new StringBuilder();
-		int end = s.length() - 1;
-		int start = 0;
-		while (end >= 0) {
-			if (s.charAt(end) != ' ') {
-				for (start = end; start >= 0; start--) {
-					if (s.charAt(start) != ' ') {
-						continue;
-					} else {
-						if (sb.length() != 0) {
-							sb.append(" ");
-						}
-						sb.append(s.subSequence(start + 1, end + 1));
-						end = start;
-						break;
-					}
-				}
-				if (start < 0) {
-					if (sb.length() != 0) {
-						sb.append(" ");
-					}
-					sb.append(s.subSequence(start + 1, end + 1));
-					break;
-				}
-			}
-			end--;
+		if (startIndex > endIndex || startIndex < 0 || endIndex < 0) {
+			return s;
 		}
-		return sb.toString();
+		char temp = 0;
+		char[] temp_s = s.toCharArray();
+		while (startIndex < endIndex) {
+			temp = temp_s[startIndex];
+			temp_s[startIndex] = temp_s[endIndex];
+			temp_s[endIndex] = temp;
+			startIndex++;
+			endIndex--;
+		}
+		return String.copyValueOf(temp_s);
 	}
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println("\"" + solution.reverseWords("sdf fwerr  ") + "\"");
+		String test = "abc   def";
+		System.out.println("\""
+				+ solution.reverseString(test, 0, test.length() - 1) + "\"");
+		System.out.println("\"" + solution.reverseWords(test) + "\"");
 	}
 }
