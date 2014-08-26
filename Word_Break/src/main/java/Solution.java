4716619fde9c7263745b2d37af70050e62556486
Kai Chen <allench86@gmail.com>
2014-08-25 23:45:22 -0700

    Word Break
    Approach 1: Dynamic Programming approach. O(n^2) time and O(n) spaces.
    Have the offset starts from 1 to s.length, check the s.subString(start,
    offset) is in the dictionary or not. If yes, save the end
    point(offset). For next offset, loop through all the saved end points,
    have each end point as the start, and check s.subString(start, offset)
    in the dictionary or not. If yes, save the new end point.
    
    After the loop, check if the end point is the length of s or not. If
    yes, that means there is a match.

---
+++ Word_Break/src/main/java/Solution.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Set;
+
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null || dict == null) {
+            return false;
+        }
+        LinkedList<Integer> starts = new LinkedList<Integer>();
+        starts.add(0);
+
+        for (int end = 1; end <= s.length(); end++) {
+            boolean found = false;
+            for (Integer start : starts) {
+                if (dict.contains(s.subSequence(start, end))) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                starts.add(0, end);
+            }
+        }
+
+        return starts.get(0) == s.length();
+    }
+}
