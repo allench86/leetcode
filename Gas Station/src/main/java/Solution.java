8762f2edc57139ec19580f196a49d70e90a7e2a5
Kai Chen <allench86@gmail.com>
2014-09-08 21:54:01 -0700

    LeetCode: Gas Station
    Approach 2: DP approach

--- Gas Station/src/main/java/Solution.java
+++ Gas Station/src/main/java/Solution.java
@@ -1,33 +1,29 @@
 public class Solution {
     public static void main(String[] args) {
-        int[] gas = { 4 };
-        int[] cost = { 5 };
+        int[] gas = { 5 };
+        int[] cost = { 4 };
 
         Solution s = new Solution();
         System.out.println(s.canCompleteCircuit(gas, cost));
     }
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int result = -1;
+        int result = 0;
+        int total = 0;
+        int currentGas = 0;
 
         for (int i = 0; i < gas.length; i++) {
-            int current = i;
-            int carryGas = 0;
-            int costGas = 0;
-            int j = 0;
-            for (; j < gas.length; j++) {
-                carryGas += gas[current];
-                costGas += cost[current];
-                if (costGas > carryGas) {
-                    break;
-                }
-                current = (current + 1 + gas.length) % gas.length;
+            int remain = gas[i] - cost[i];
+            if (currentGas >= 0) {
+                currentGas += remain;
             }
-            if (j == gas.length) {
+            else {
+                currentGas = remain;
                 result = i;
-                break;
             }
+            total += remain;
         }
-        return result;
+
+        return total >= 0 ? result : -1;
     }
 }
