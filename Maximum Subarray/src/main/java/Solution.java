a65e84c2eeb34d34a1368b6a2eaf3d4701adf34e
Kai Chen <allench86@gmail.com>
2014-10-26 16:17:59 -0700

    LeetCode: Max Subarray
    Approach 1: Dynamical programming: We should ignore the sum of the
    previous n-1 elements if nth element is greater than the sum.

---
+++ Maximum Subarray/src/main/java/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = { -1 };
+        System.out.println(solution.maxSubArray(A));
+    }
+
+    public int maxSubArray(int[] A) {
+        int max = A[0];
+        int[] sum = new int[A.length];
+        sum[0] = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            sum[i] = Math.max(A[i], sum[i - 1] + A[i]);
+            max = Math.max(max, sum[i]);
+        }
+
+        return max;
+    }
+}
