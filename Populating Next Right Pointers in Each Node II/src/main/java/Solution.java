97adaf89e3c99356629a39ef690425f251ca1673
Kai Chen <allench86@gmail.com>
2014-11-18 22:23:56 -0800

    LeetCode: Populating Next Right Pointers in Each Node II

---
+++ Populating Next Right Pointers in Each Node II/src/main/java/Solution.java
@@ -0,0 +1,76 @@
+public class Solution {
+
+    public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+
+        root.next = null;
+
+        TreeLinkNode headOfCurrentLavel = root;
+        TreeLinkNode currentParent = root;
+
+        while (headOfCurrentLavel != null) {
+            while (currentParent != null) {
+                if (currentParent.left != null && currentParent.right != null) {
+                    currentParent.left.next = currentParent.right;
+                }
+                if (currentParent.left != null && currentParent.right == null) {
+                    TreeLinkNode next = currentParent.next;
+                    while (next != null) {
+                        if (next.left != null) {
+                            currentParent.left.next = next.left;
+                            break;
+                        }
+                        else if (next.right != null) {
+                            currentParent.left.next = next.right;
+                            break;
+                        }
+                        else {
+                            currentParent.left.next = null;
+                            next = next.next;
+                        }
+                    }
+                }
+                if (currentParent.right != null) {
+                    TreeLinkNode next = currentParent.next;
+                    while (next != null) {
+                        if (next.left != null) {
+                            currentParent.right.next = next.left;
+                            break;
+                        }
+                        else if (next.right != null) {
+                            currentParent.right.next = next.right;
+                            break;
+                        }
+                        else {
+                            currentParent.right.next = null;
+                            next = next.next;
+                        }
+                    }
+                }
+
+                currentParent = currentParent.next;
+            }
+            if (headOfCurrentLavel.left != null) {
+                currentParent = headOfCurrentLavel.left;
+            }
+            else if (headOfCurrentLavel.right != null) {
+                currentParent = headOfCurrentLavel.right;
+            }
+            else {
+                currentParent = null;
+            }
+            headOfCurrentLavel = currentParent;
+        }
+    }
+
+    public static class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
+}
