d5cfe5c7fafb53e2159e5bc20280ab08bd2e150f
Kai Chen <allench86@gmail.com>
2014-12-02 22:29:40 -0800

    LeetCode: Best Time to Buy and Sell Stock
    Approach 1: Dynamical programming: MaxProfit[i] = Max( MaxProfit[i-1],
    prices[i] - minPrice). minPrice is the lowest price within range
    price[0] to price[i-1].

--- Best Time to Buy and Sell Stock/src/main/java/Solution.java
+++ Best Time to Buy and Sell Stock/src/main/java/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public static void main(String[] args) {
-        int[] prices = { 3, 2, 6, 5, 0, 3 };
+        int[] prices = { 3, 2, 6, 8, 0, 3 };
         Solution s = new Solution();
 
         System.out.println(s.maxProfit(prices));
@@ -8,39 +8,16 @@ public class Solution {
 
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
-        if (prices.length > 0) {
-            int maxIndex = prices.length - 1, minIndex = 0;
-            int max = prices[maxIndex];
+        if (prices.length > 1) {
+            maxProfit = prices[1] - prices[0];
+            int minPrice = prices[0];
 
-            do {
-                minIndex = maxIndex - 1;
-                int min = prices[minIndex > 0 ? minIndex : 0];
-                while (minIndex > 0 && prices[minIndex] > prices[maxIndex]) {
-                    minIndex--;
-                    maxIndex--;
-                }
-
-                if (max > prices[maxIndex] && maxIndex != prices.length - 1) {
-                    maxIndex = minIndex - 1;
-                    continue;
-                }
-                max = prices[maxIndex];
-
-                while (minIndex >= 0) {
-                    while (minIndex - 1 >= 0 && prices[minIndex] > prices[minIndex - 1]) {
-                        minIndex--;
-                    }
-
-                    if (minIndex >= 0 && maxIndex >= 0 && min >= prices[minIndex]) {
-                        maxProfit = Math.max(maxProfit, prices[maxIndex] - prices[minIndex]);
-                        min = prices[minIndex];
-                    }
-                    minIndex--;
-                }
-                maxIndex = maxIndex - 1;
-            } while (maxIndex > 0);
+            for (int i = 2; i < prices.length; i++) {
+                minPrice = Math.min(prices[i - 1], minPrice);
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
         }
 
-        return maxProfit;
+        return maxProfit > 0 ? maxProfit : 0;
     }
 }
