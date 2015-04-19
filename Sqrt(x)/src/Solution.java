a9574cc0a8420c811b56aa58c4d25a3c311cba51
Kai Chen <allench86@gmail.com>
2015-04-19 15:16:16 -0700

    LeetCode: Squrt(x)
    Approach 2: 牛顿迭代法。http://en.wikipedia.org/wiki/Newton%27s_method

--- Sqrt(x)/src/Solution.java
+++ Sqrt(x)/src/Solution.java
@@ -3,29 +3,12 @@ public class Solution {
         if (x == 0 || x == 1) {
             return x;
         }
-        int i = 0;
-        int j = x / 2 + 1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (mid != 0) {
-                if (mid * mid / mid == mid) {
-                    int sq = mid * mid;
-                    if (sq == x) {
-                        return mid;
-                    }
-                    else if (sq < x) {
-                        i = mid + 1;
-                    }
-                    else {
-                        j = mid - 1;
-                    }
-                }
-                else {
-                    j = mid - 1;
-                }
-            }
+        double last = 0;
+        double next = 1;
+        while (next != last) {
+            last = next;
+            next = (next + x / next) / 2;
         }
-
-        return (i + j) / 2;
+        return (int) next;
     }
 }
