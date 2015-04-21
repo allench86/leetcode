4e225d55924bd94389e81ae4f6842f8c46f8fc36
Kai Chen <allench86@gmail.com>
2015-04-20 20:32:53 -0700

    LeetCode: Search Insert Position
    Approach 2: Binary search

--- Search Insert Position/src/main/java/Solution.java
+++ Search Insert Position/src/main/java/Solution.java
@@ -1,25 +1,27 @@
 public class Solution {
     public static void main(String[] args) {
-        int[] A = { 1, 3, 5 };
-        int target = 6;
+        int[] A = { 3 };
+        int target = 3;
         Solution s = new Solution();
         System.out.println(s.searchInsert(A, target));
     }
 
     public int searchInsert(int[] A, int target) {
-        int i = 0;
-        while (i < A.length) {
-            if (target == A[i]) {
-                break;
-            }
-            else if (target < A[i]) {
-                break;
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int r = 0;
+        int l = A.length - 1;
+        while (r <= l) {
+            int mid = (r + l) / 2;
+            if (A[mid] >= target) {
+                l = mid - 1;
             }
             else {
-                i++;
+                r = mid + 1;
             }
         }
 
-        return i;
+        return r;
     }
 }
