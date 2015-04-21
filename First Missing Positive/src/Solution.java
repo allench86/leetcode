ac87b1a8ed2c24e7f409524d1cf97d1500233639
Kai Chen <allench86@gmail.com>
2015-04-20 21:41:51 -0700

    LeetCode: First Missing Positive
    Approach 1: Use the passed in array as a hash map.
    1. If A[i] <1 or A[i] > A.length, it is not the result
    2. If A[i] == i+1, it is in the right place, it is not the result
    3. If A[i] != i+1 and A[i] > 0 and A[i] <= A.length and A[i] !=
    A[A[i]-1] then move A[i] to the right location which should be A[i-1].
    So do a swap of A[i-1] and A[A[i]-1]

---
+++ First Missing Positive/src/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < 1 || nums[i] > nums.length || nums[i] == i + 1 || nums[i] == nums[nums[i] - 1]) {
+                i++;
+            }
+            else {
+                int t = nums[i];
+                nums[i] = nums[t - 1];
+                nums[t - 1] = t;
+            }
+        }
+
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
