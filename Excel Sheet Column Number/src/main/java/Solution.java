7c6b200b73496ad6e27cbf84aa3e255070bd24d4
Kai Chen <allench86@gmail.com>
2015-04-20 20:24:06 -0700

    LeetCode: Excel Sheet Column Number
    Approach 2

--- Excel Sheet Column Number/src/main/java/Solution.java
+++ Excel Sheet Column Number/src/main/java/Solution.java
@@ -3,11 +3,10 @@ public class Solution {
         int result = 0;
         if (s != null) {
             char[] chars = s.toCharArray();
-            int i = chars.length - 1;
-            for (char c : chars) {
+            for (int i = 0; i < chars.length; i++) {
+                char c = chars[i];
                 int offset = c - 'A' + 1;
-                result += offset * Math.pow(26, i);
-                i--;
+                result = result * 26 + offset;
             }
         }
         return result;
